71f56400a53efe92eddfc2959dd73826e978b5cf
LionEnterprise <usalasabbas@gmail.com>
2024-09-25 16:20:54 -0400

    paky

--- TP2.py
+++ TP2.py
@@ -13,18 +13,18 @@ Noms et matricules : Abbas, Usalas (2383986), Nguyen Le, Williamg (2393842)
 # TODO : Écrire votre code ici
 import csv
 
-import csv
 
 csvfile = open('collection_bibliotheque.csv', newline='')
 c = csv.reader(csvfile)
 collection_livre = []
-print(type(collection_livre))
 
 for row in c:
-    print(row)
-    #collection_livre.append[row]
+   # print(row)
+    collection_livre.append(row)
 
 csvfile.close()
+
+print(collection_livre)
 
 ########################################################################################################## 
 # PARTIE 2 : Ajout d'une nouvelle collection à la bibliothèque
