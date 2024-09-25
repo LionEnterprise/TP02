d47472a8b92b6e0f70b7f1c2c51c7e1f382ba440
LionEnterprise <usalasabbas@gmail.com>
2024-09-25 16:13:07 -0400

    debut partie 1

--- TP2.py
+++ TP2.py
@@ -3,7 +3,7 @@ TP2 : Système de gestion de livres pour une bibliothèque
 
 Groupe de laboratoire : 01
 Numéro d'équipe :  03
-Noms et matricules : Abbas, Usalas (Matricule1), Nguyen Le, Williamg (2393842)
+Noms et matricules : Abbas, Usalas (2383986), Nguyen Le, Williamg (2393842)
 """
 
 ########################################################################################################## 
@@ -12,10 +12,18 @@ Noms et matricules : Abbas, Usalas (Matricule1), Nguyen Le, Williamg (2393842)
 
 # TODO : Écrire votre code ici
 
+import csv
 
+csvfile = open('collection_bibliotheque.csv', newline='')
+c = csv.reader(csvfile)
+collection_livre = []
+print(type(collection_livre))
 
+for row in c:
+    print(row)
+    #collection_livre.append[row]
 
-
+csvfile.close()
 
 ########################################################################################################## 
 # PARTIE 2 : Ajout d'une nouvelle collection à la bibliothèque
