fa71b7bf0bd90aa499b41d25f49215fb491d60b5
Bichdung <william.nguyen.le06@gmail.com>
2024-09-30 15:54:14 -0400

    Partie 1 Fini

--- TP2.py
+++ TP2.py
@@ -11,47 +11,24 @@ Noms et matricules : Abbas, Usalas (Matricule1), Nguyen Le, William (2393842)
 ########################################################################################################## 
 
 # TODO : Écrire votre code ici
-
 import csv
 
-dictionnaire = {}
-collection = {}
-csvfile = open('collection_bibliotheque.csv')
+bibliotheque = {}
+
+csvfile = open("collection_bibliotheque.csv")
 c = csv.reader(csvfile)
-#bibliotheque = []
-#titre = []
-#auteur = []
-#date_publication = []
-#cote_rangement = []
+next(c)
 for row in c:
-    cle = row[3]
-    titre = row[0]
-    auteur = row[1]
-    date_publication = row[2]
-    collection["titre"] = titre
-    collection["auteur"] = auteur
-    collection["date_publication"] = date_publication
-    #titre.append(row[0])
-    #auteur.append(row[1])
-    #date_publication.append(row[2])
-    #cote_rangement.append(row[3])
-    #bibliotheque.append(row)
-    dictionnaire[cle] = collection
-    #print(collection)
-    #for key, val in dict.items(collection):
-    #    dictionnaire[key] = val
+    titre, auteur, date_publication, cote_rangement = row
+    bibliotheque[cote_rangement] = {
+        "titre" : titre,
+        "auteur" : auteur,
+        "date_publication" : date_publication
+    }
 csvfile.close()
-print(dictionnaire)
-#print(dictionnaire["P021"])
-#print(collection)
-print(f' \n Bibliotheque initiale : {dictionnaire} \n')
-csvfile.close()
-
-#csvfile = open('Bibliothèque', 'w')
-
-
-
-
+print(bibliotheque["P021"])
+print(bibliotheque["H007"])
+print(f' \n Bibliotheque initiale : {bibliotheque} \n')
 
 ########################################################################################################## 
 # PARTIE 2 : Ajout d'une nouvelle collection à la bibliothèque
